bdd5db90daa736c820105fc9e6b91d1ce6a8efff
praneu <prazolbikneup@gmail.com>
2023-06-01 11:58:07 -0400

    Changes in Components

---
+++ src/app/department-list/department-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'department-list',
+  template: `
+  <h3>Department</h3>
+  
+  `,
+  styles: [``]
+})
+export class DepartmentListComponent {
+
+}
---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  template: `
+  <h3>employees</h3>
+  `,
+  styles: [``]
+})
+export class EmployeeListComponent {
+
+}
