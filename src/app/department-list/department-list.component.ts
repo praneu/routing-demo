39c9e004de51e15a7e7e9623304eaf2e22ef9f89
praneu <prazolbikneup@gmail.com>
2023-06-03 19:29:22 -0400

    Changes in Styles and Added parameter to routes

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'department-list',
   template: `
-  <h2>Department</h2>
-  <ul>
-    <li>Front of the House</li>
-    <li>Kitchen</li>
-    <li>Valet</li>
-    <li>Human Resource</li>
+  <h2>Department List</h2>
+  <ul class="items">
+    <li (click)="onSelect(department)" *ngFor="let department of departments">
+      <span class="badge">{{department.id}}</span> {{department.name}}
+    </li>
   </ul>  
-  
   `,
   styles: [``]
 })
 export class DepartmentListComponent {
+
+  departments =[
+    { "id": 1, "name": "Angular"},
+    { "id": 2, "name": "Node"},
+    { "id": 3, "name": "MongoDB"},
+    { "id": 4, "name": "Ruby"},
+    { "id": 5, "name": "Bootstrap"}
+  ]
+
+  constructor(private router: Router) {}
+
+  onSelect(department: { id: any; }){
+     this.router.navigate(['/departments', department.id]);
+  }
 
 }
